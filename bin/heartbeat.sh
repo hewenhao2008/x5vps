27749317319f71a5bcd1760951c111fd61253caa
link4all <279357174@qq.com>
2021-11-01 03:45:27 -0400

    serverip auto detect

--- bin/heartbeat.sh
+++ bin/heartbeat.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+location="huanan"
+ip="`curl -s cip.cc |grep IP|cut -d" " -f2`"
+
 url="gl.mine.com.cn"
 path="/api/device/postserverinfo"
 path2="/api/device/checkstatus"
 
-ip="121.37.243.25"
-location="huanan"
 
 header='Content-type':'application/json'
 poststr="{\"ip\":\"$ip\",\"locate\":\"$location\",\"vpnport\":{\"startport\":60000,\"endport\":60009},\"fixports\":[{\"fixport\":60010}]}"
